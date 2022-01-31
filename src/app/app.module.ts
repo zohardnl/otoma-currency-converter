092d014aca6604d6146599263a14e98914c86799
daniel.zohar <daniel.zohar@otoma.com>
2022-01-31 11:25:15 +0200

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
 import { ConverterComponent } from './converter/converter.component';
 import { HistoryComponent } from './history/history.component';
 
@@ -16,7 +15,6 @@ import { HistoryComponent } from './history/history.component';
     BrowserModule,
     AppRoutingModule
   ],
-  exports: [RouterModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
